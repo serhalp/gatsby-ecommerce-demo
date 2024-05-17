97a830e6e988dbcef6933086402b51141a96d4d7
Philippe Serhal <philippe.serhal@netlify.com>
2024-05-17 10:07:16 -0400

    add static redirect rule

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,11 +4,20 @@ exports.onCreateWebpackConfig = (helper) => {
   if (stage === 'develop' || stage === 'build-javascript') {
     const config = getConfig();
     const miniCssExtractPlugin = config.plugins.find(
-      (plugin) => plugin.constructor.name === 'MiniCssExtractPlugin'
+      (plugin) => plugin.constructor.name === 'MiniCssExtractPlugin',
     );
     if (miniCssExtractPlugin) {
       miniCssExtractPlugin.options.ignoreOrder = true;
     }
     actions.replaceWebpackConfig(config);
   }
+};
+
+exports.createPages = async ({ actions }) => {
+  const { createRedirect } = actions;
+
+  createRedirect({
+    fromPath: `/blog/crample`,
+    toPath: `/blog/sample`,
+  });
 };
